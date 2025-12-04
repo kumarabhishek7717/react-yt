24cc32e9666e00579bd6da5427e31c0ae6e05a5c
kumarabhishek7717 <abhishekkumar7717749@gmail.com>
2025-12-04 13:06:22 +0530

    usestate and counter button

---
+++ usestate-09/src/App.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from 'react'
+
+const App = () => {
+ 
+ const [num , setNum] = useState(0);
+
+ function increase(){
+      setNum(num + 1)
+ }
+
+ function decrease(){
+    setNum(num - 1)
+ }
+  
+ 
+ function increase5(){
+    setNum(num +5)
+ }
+
+
+  return (
+    <div>
+      <h1>{num}</h1>
+      <button onClick={increase}>increase</button>
+      <button onClick={decrease}>decrease</button>
+      <button onClick={increase5}>increase5</button>
+    </div>
+  )
+}
+
+export default App
