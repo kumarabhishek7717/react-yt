477ce62f1f76f1f8b2e7c35d31a36d2e8bdfdcee
kumarabhishek7717 <abhishekkumar7717749@gmail.com>
2025-12-09 00:47:28 +0530

    gallery project done

---
+++ 17-gallery-project/src/App.jsx
@@ -0,0 +1,69 @@
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+
+const App = () => {
+
+  const[userData, setUserData] = useState([]);
+  const[index, setIndex] = useState(1)
+ 
+  const getData = async () =>{
+    const response = await axios.get(`https://picsum.photos/v2/list?page=${index}&limit=50`)
+      setUserData(response.data)
+      
+  }
+
+  useEffect(function(){
+    getData()
+
+  },[index])
+    
+  let printUserData = <h3 className='text-gray-400 absolute top-1/2 left-1/2 -translate-x1/2 -translate-y-1/2'>Loading...</h3>
+
+  if (userData.length>0){
+    printUserData = userData.map(function(elem,idx){
+      return <div key={idx}>
+        <a href={elem.url} target='_blank'>
+          <div className='h-40 w-44 overflow-hidden bg-white rounded-xl'>
+        <img className='h-[82%] w-full object-cover' src={elem.download_url} alt="" />
+      </div>
+      <h2 className='font-bold text-lg'>{elem.author}</h2>
+        </a>
+        </div>
+    })
+  }
+
+
+  return (
+    <div className='bg-black overflow-auto h-screen p-4 text-white'>
+       <h1 className='fixed text-6xl bg-red-600'>{index}</h1>
+       <div className='flex flex-wrap gap-4'>
+        {printUserData}
+       </div>
+
+       <div className='flex justify-center gap-6 items-center p-4'>
+        <button 
+        style={{opacity:index == 1 ? 0.6 : 1 }}
+        className='bg-amber-400 text-black text-sm cursor-pointer active:scale-95 rounded px-4 py-2 font-semibold '
+        onClick={()=>{
+          if(index>1)
+           setIndex(index-1)
+          setUserData([])
+        }
+        }
+        >Prev</button>
+        <h4>Page {index}</h4>
+        <button 
+        className='bg-amber-400 text-black text-sm cursor-pointer active:scale-95 rounded px-4 py-2 font-semibold'
+        onClick={()=>{
+          setIndex(index+1)
+          setUserData([])
+        }}
+        >Next</button>
+       </div>
+
+
+    </div>
+  )
+}
+
+export default App
