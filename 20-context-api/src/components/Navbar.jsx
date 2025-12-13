2e3e594a936a2238d515624cca494dbe7418a8e7
kumarabhishek7717 <abhishekkumar7717749@gmail.com>
2025-12-13 15:04:43 +0530

    Bonus Topic

---
+++ 20-context-api/src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Navbar = (props) => {
+    function changeTheme(){ 
+        props.setTheme('Dark')
+        
+    }
+    
+  return (
+    <div>
+     
+      <button onClick={changeTheme}
+      >Change Theme</button>
+    </div>
+  )
+}
+
+export default Navbar
